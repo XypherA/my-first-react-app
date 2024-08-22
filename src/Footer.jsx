3086124e2e968efefb8b42eacc7d7df10b4c0cfc
XypherA <aiemanshazwannPG@gmail.com>
2024-08-22 19:40:08 +0800

    Changes are made

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,29 +1,35 @@
 
 function Footer(){
     return(
-    <nav>
-        <ul>
-            <h2>Doormat navigation</h2>
-            <li><a href="">HOME</a></li>
-            <li><a href="">ABOUT</a></li>
-            <li><a href="">MENU</a></li>
-            <li><a href="">RESERVATION</a></li>
-            <li><a href="">ORDER ONLINE</a></li>
-            <li><a href="">LOGIN</a></li>
-        </ul>
-        <ul>
-            <h2>Contact</h2>
-            <li>Address</li>
-            <li>Phone Number</li>
-            <li>Email</li>
-        </ul>
-        <ul>
-            <h2>Social Media Links</h2>
-            <li><a href="">Facebook</a></li>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Tiktok</a></li>
-        </ul>
-    </nav>
+    <footer className="footer">
+        <div className="footer-column">
+            <h3 className="footer-tittle">Doormat navigation</h3>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">ABOUT</a></li>
+                <li><a href="">MENU</a></li>
+                <li><a href="">RESERVATION</a></li>
+                <li><a href="">ORDER ONLINE</a></li>
+                <li><a href="">LOGIN</a></li>
+            </ul>
+        </div>
+        <div className="footer-column">
+            <h3 className="footer-tittle">Contact</h3>
+            <ul>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+        </div>
+        <div className="footer-column">
+            <h3 className="footer-tittle">Social Media Links</h3>
+            <ul>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Tiktok</a></li>
+            </ul>
+        </div>
+    </footer>
     )
 }
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,8 @@
 
 function Nav(){
     return(
-        <nav>
-            <ul>
+        <nav className="navbar">
+            <ul className="nav-links">
                 <li><a href="">HOME</a></li>
                 <li><a href="">ABOUT</a></li>
                 <li><a href="">MENU</a></li>
